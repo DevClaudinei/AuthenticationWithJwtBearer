891902d7c33034c0d34460dbc1048329458267aa
Claudinei Santos <claudinei.santos@warren.com.br>
2022-11-28 10:49:09 -0300

    feat: criacao de profile

---
+++ ProjetoLoginToken/Profiles/UserProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ProjetoLoginToken.Models;
+using ProjetoLoginToken.Models.Requests;
+
+namespace ProjetoLoginToken.Profiles;
+
+public class UserProfiles : Profile
+{
+	public UserProfiles()
+	{
+		CreateMap<RegisterRequest, User>();
+    }
+}
