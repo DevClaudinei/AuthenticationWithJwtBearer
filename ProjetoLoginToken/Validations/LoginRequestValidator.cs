59ea45ed9bad7a442fb659c490a346831a42fdbf
Claudinei Santos <claudinei.santos@warren.com.br>
2022-11-28 10:49:36 -0300

    feat: criacao de validations

---
+++ ProjetoLoginToken/Validations/LoginRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using ProjetoLoginToken.Models.Requests;
+
+namespace ProjetoLoginToken.Validations;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Username)
+            .NotEmpty()
+            .NotNull()
+            .MinimumLength(8);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .NotNull()
+            .MinimumLength(6);
+    }
+}
---
+++ ProjetoLoginToken/Validations/RegisterRequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using ProjetoLoginToken.Models.Requests;
+
+namespace ProjetoLoginToken.Validations;
+
+public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+	public RegisterRequestValidator()
+	{
+		RuleFor(x => x.Username)
+            .NotEmpty()
+            .NotNull()
+            .MinimumLength(8);
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .NotNull()
+            .EmailAddress(EmailValidationMode.Net4xRegex);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(6);
+
+        RuleFor(x => x)
+            .Must(x => x.Password == x.ConfirmPassword);
+    }
+}
