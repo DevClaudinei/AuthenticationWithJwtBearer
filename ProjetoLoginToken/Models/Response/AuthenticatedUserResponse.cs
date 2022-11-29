bb0d6a1f0ff9ebf88b2606a7cbacf44ff01287b9
Claudinei Santos <claudinei.santos@warren.com.br>
2022-11-29 18:07:37 -0300

    refactor: criacao de construtores para dto user response

--- ProjetoLoginToken/Models/Response/AuthenticatedUserResponse.cs
+++ ProjetoLoginToken/Models/Response/AuthenticatedUserResponse.cs
@@ -2,5 +2,12 @@
 
 public class AuthenticatedUserResponse
 {
+	public AuthenticatedUserResponse() { }
+
+	public AuthenticatedUserResponse(string acessToken)
+	{
+		AcessToken = acessToken;
+	}
+
     public string AcessToken { get; set; }
 }
