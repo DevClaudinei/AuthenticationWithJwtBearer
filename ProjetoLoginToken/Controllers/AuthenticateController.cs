990c4f976ad5c959f6035942a688960dfc3e65ff
Claudinei Santos <claudinei.santos@warren.com.br>
2022-11-29 18:06:35 -0300

    refactor: insercao de novos metodos na controller

--- ProjetoLoginToken/Controllers/AuthenticateController.cs
+++ ProjetoLoginToken/Controllers/AuthenticateController.cs
@@ -1,9 +1,9 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoLoginToken.Models;
 using ProjetoLoginToken.Models.Requests;
 using ProjetoLoginToken.Services.Interfaces;
-using ProjetoLoginToken.Services.TokenGenerator;
 using System;
 using System.Threading.Tasks;
 
@@ -15,13 +15,11 @@ public class AuthenticateController : ControllerBase
 {
     private readonly IMapper _mapper;
     private readonly IUserService _userService;
-    private readonly AcessTokenGenerator _acessTokenGenerator;
 
-    public AuthenticateController(IUserService userService, IMapper mapper, AcessTokenGenerator acessTokenGenerator)
+    public AuthenticateController(IUserService userService, IMapper mapper)
     {
-        _mapper = mapper ?? throw new System.ArgumentNullException(nameof(mapper));
-        _userService = userService ?? throw new System.ArgumentNullException(nameof(userService));
-        _acessTokenGenerator = acessTokenGenerator ?? throw new ArgumentNullException(nameof(acessTokenGenerator));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _userService = userService ?? throw new ArgumentNullException(nameof(userService));
     }
 
     [HttpPost("register")]
@@ -47,13 +45,34 @@ public class AuthenticateController : ControllerBase
         try
         {
             var user = await _userService.Login(loginRequest);
-            var acessToken = _acessTokenGenerator.GenerateToken(user);
-
-            return Ok(acessToken);
+            return Ok(user);
         }
         catch
         {
             return Unauthorized();
         }
+    }
+
+    [Authorize]
+    [HttpGet("id")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        try
+        {
+            var acessToken = await _userService.GetByIdAsync(id);
+            return Ok(acessToken);
+        }
+        catch (ArgumentException exception)
+        {
+            var message = exception.InnerException?.Message ?? exception.Message;
+            return BadRequest(message);
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var users = await _userService.GetAll();
+        return Ok(users);
     }
 }
